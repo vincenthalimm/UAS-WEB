c93fb535b28a64e4598287dac0e71b9d854f4270
vincenthalimm <vincenthalim10456@gmail.com>
2026-07-05 12:35:21 +0700

    Menggunakan dotenv

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
-import mysql from "mysql2/promise"
+import "dotenv/config";
+import mysql from "mysql2/promise";
 
 const db = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "110906v&H",
-    database: "keuangan"
-})
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
-console.log("✅ Database connected!")
+console.log("✅ Database connected!");
 
-export default db
+export default db;
